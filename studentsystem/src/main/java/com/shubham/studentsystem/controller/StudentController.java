c04edd8ec12dae25184387736172620f4c545f1d
snavale2 <snavale@binghamton.edu>
2024-05-27 01:03:43 -0400

    Implemented CRUD

--- studentsystem/src/main/java/com/shubham/studentsystem/controller/StudentController.java
+++ studentsystem/src/main/java/com/shubham/studentsystem/controller/StudentController.java
@@ -1,4 +1,28 @@
 package com.shubham.studentsystem.controller;
 
+import com.shubham.studentsystem.model.Student;
+import com.shubham.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+@CrossOrigin
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New Student Added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
 }
--- studentsystem/src/main/java/com/shubham/studentsystem/service/StudentService.java
+++ studentsystem/src/main/java/com/shubham/studentsystem/service/StudentService.java
@@ -1,4 +1,10 @@
 package com.shubham.studentsystem.service;
 
+import com.shubham.studentsystem.model.Student;
+
+import java.util.List;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
